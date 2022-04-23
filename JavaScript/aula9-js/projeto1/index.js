a0def595f78f39862b82f2250c3ca27cc402d02e
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-04-22 21:14:12 -0300

    atualiza-js

---
+++ JavaScript/aula9-js/projeto1/index.js
@@ -0,0 +1,8 @@
+let i = 1;
+let resultado = 1;
+
+while (resultado <=10){
+    console.log(resultado);
+    resultado++;
+
+}
---
+++ JavaScript/aula9-js/projeto3/index.js
@@ -0,0 +1,9 @@
+let i = 1;
+let resultado = 0;
+let flag = true;
+
+do {
+    console.log(resultado);
+    resultado++;
+
+}while(resultado <= 10);
