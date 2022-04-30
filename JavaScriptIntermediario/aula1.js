70c5cfa1789321e7e8b9aad3e5738115abb488ba
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-04-30 11:01:30 -0300

    aulas javascript intermediário

---
+++ JavaScriptIntermediario/aula1.js
@@ -0,0 +1,10 @@
+var idade = 18;
+console.log(idade >= 18 ? 'maior de idade' : 'menor de idade');
+
+
+var tarefa = false;
+console.log(tarefa === true ? 'Usuário Deseja realizar tarefa ou Alguém permitiu a tarefa' : 'condição não atendida' );
+
+
+var clima = 'chuva';
+console.log(clima === 'sol'? 'Roupa Leve': clima === 'chuva' ? 'Usar capa de chuva': clima === 'frio' ? 'Usar roupa de frio' : 'usar qualquer roupa');
---
+++ JavaScriptIntermediario/aula2.js
@@ -0,0 +1,33 @@
+
+//Exercício 1
+let valor = 3;
+
+let par = () => {
+    if (valor %2 === 0){
+        return 'o valor é par';
+    }else{
+        return 'o valor é impar';
+    }
+}
+
+// Exercício 2
+console.log(par());
+
+let numberOne = 5;
+let numberTwo = 7;
+
+let soma = (numberOne, numberTwo) => numberOne + numberTwo;
+
+console.log(soma(numberOne, numberTwo));
+
+// Exercício 3
+
+let cumprimento = true;
+let aux = 0;
+
+let retorno = () => {
+    aux = cumprimento === true ? 'Está feliz' : 'Está triste!';
+    return aux;
+}
+
+console.log(retorno());
