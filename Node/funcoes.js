b8d085ffc1511a9150a6eff0adbbbbe1cf2c7994
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-05-05 22:10:22 -0300

    Node P1

---
+++ Node/funcoes.js
@@ -0,0 +1,17 @@
+let numOne = 0;
+let numTwo = 0;
+
+const sum = ( numOne,  numTwo) => numOne + numTwo;
+const sub = (numOne,  numTwo) => numOne - numTwo;
+const mult = (numOne,  numTwo) => numOne * numTwo;
+const div = (numOne,  numTwo) => numOne / numTwo;
+
+module.exports = {
+   sum,
+   sub,
+   mult,
+   div
+
+}
+
+//console.log(sum(1,2));
