59950927886516402197130c00551a1a32ddc39d
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-05-11 20:28:02 -0300

    atualizando repositório

--- Node/aula-online2.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-setTimeout(() => console.log('Hello'), 10);
-
-fetch('https://gorest.co.in/public/v2/users')
-    .then(response => response.json())
-    .then (data => console.log(data))
-
-console.log('What soup');
-
